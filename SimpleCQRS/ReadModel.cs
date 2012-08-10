02be836965478829a4d0c9bce6fc75260f4c909e
Marijn van der Zee <marijn@serraict.com>
2012-08-10 13:46:41 +0200

    Remove old read model builders that use in-memory db

--- SimpleCQRS/ReadModel.cs
+++ SimpleCQRS/ReadModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using SimpleCQRS.Events;
 
 namespace SimpleCQRS
 {
@@ -43,87 +44,5 @@ namespace SimpleCQRS
             Id = id;
             Name = name;
         }
-    }
-
-    public class InventoryListView : Handles<InventoryItemCreated>, Handles<InventoryItemRenamed>, Handles<InventoryItemDeactivated>
-    {
-        public void Handle(InventoryItemCreated message)
-        {
-            BullShitDatabase.list.Add(new InventoryItemListDto(message.Id, message.Name));
-        }
-
-        public void Handle(InventoryItemRenamed message)
-        {
-            var item = BullShitDatabase.list.Find(x => x.Id == message.Id);
-            item.Name = message.NewName;
-        }
-
-        public void Handle(InventoryItemDeactivated message)
-        {
-            BullShitDatabase.list.RemoveAll(x => x.Id == message.Id);
-        }
-    }
-
-    public class InvenotryItemDetailView : Handles<InventoryItemCreated>, Handles<InventoryItemDeactivated>, Handles<InventoryItemRenamed>, Handles<ItemsRemovedFromInventory>, Handles<ItemsCheckedInToInventory>
-    {
-        public void Handle(InventoryItemCreated message)
-        {
-            BullShitDatabase.details.Add(message.Id, new InventoryItemDetailsDto(message.Id, message.Name, 0,0));
-        }
-
-        public void Handle(InventoryItemRenamed message)
-        {
-            InventoryItemDetailsDto d = GetDetailsItem(message.Id);
-            d.Name = message.NewName;
-            d.Version = message.Version;
-        }
-
-        private InventoryItemDetailsDto GetDetailsItem(Guid id)
-        {
-            InventoryItemDetailsDto d;
-            if(!BullShitDatabase.details.TryGetValue(id, out d))
-            {
-                throw new InvalidOperationException("did not find the original inventory this shouldnt happen");
-            }
-            return d;
-        }
-
-        public void Handle(ItemsRemovedFromInventory message)
-        {
-            InventoryItemDetailsDto d = GetDetailsItem(message.Id);
-            d.CurrentCount -= message.Count;
-            d.Version = message.Version;
-        }
-
-        public void Handle(ItemsCheckedInToInventory message)
-        {
-            InventoryItemDetailsDto d = GetDetailsItem(message.Id);
-            d.CurrentCount += message.Count;
-            d.Version = message.Version;
-        }
-
-        public void Handle(InventoryItemDeactivated message)
-        {
-            BullShitDatabase.details.Remove(message.Id);
-        }
-    }
-
-    public class ReadModelFacade : IReadModelFacade
-    {
-        public IEnumerable<InventoryItemListDto> GetInventoryItems()
-        {
-            return BullShitDatabase.list;
-        }
-
-        public InventoryItemDetailsDto GetInventoryItemDetails(Guid id)
-        {
-            return BullShitDatabase.details[id];
-        }
-    }
-
-    public static class BullShitDatabase 
-    {
-        public static Dictionary<Guid, InventoryItemDetailsDto> details = new Dictionary<Guid,InventoryItemDetailsDto>();
-        public static List<InventoryItemListDto> list = new List<InventoryItemListDto>();
     }
 }
